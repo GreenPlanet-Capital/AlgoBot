03e59ebb244f69f851b7857a84340f637d7a2e1b
goldilock-zone <sarbajitghosh2002@gmail.com>
2021-05-03 12:44:20 +0530

    x

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from StockDataExtraction.StockData import SingleStockData
 from IndicatorClasses.AccumulationDistribution import AccumulationDistribution
 from IndicatorClasses.Aroon import Aroon
 
-stock_data = SingleStockData(8,True, 100)
+stock_data = SingleStockData(8,True, 200)
 stock_data.generate_dataframe()
 
-obj = Aroon(dataframe_input = stock_data.df, lookback_period = 7)
+obj = Aroon(dataframe_input = stock_data.df, lookback_period = 5)
 x,y = obj.run(9)
 print (x)
 print(y)

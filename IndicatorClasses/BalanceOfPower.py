5324e6dd9e9ad1d34454d82c5accf30beb318e33
goldilock-zone <sarbajitghosh2002@gmail.com>
2021-05-03 16:59:37 +0530

    BalanceOfPower class Constructions

--- IndicatorClasses/BalanceOfPower.py
+++ IndicatorClasses/BalanceOfPower.py
@@ -1,9 +1,9 @@
 '''
-Name: ACCUMULATION DISTRIBUTION
+Name: BALANCE OF POWER
 
 Naming Convention of DataFrame Columns: 
-    Indicator Generated DataFrame head: 
-    Signal Generated DataFrame head: 
+    Indicator Generated DataFrame head: BOP
+    Signal Generated DataFrame head: BOP SIGNAL
     Signum Generated DataFrame head: 
 
 Function List:
@@ -26,6 +26,252 @@ Function Checklist
 - current long/short strength 
 '''
 '''
-Inputs: dataframe_input, lookback_period, sensitivity = , absolute_sensitivity = 
+Inputs: dataframe_input, sensitivity = 1.3
 Outputs: weight, live_signal
 '''
+
+import math
+import pandas as pd
+import json 
+import datetime
+import numpy as np
+import sys
+import oauth2client
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.text
+
+class BalanceOfPower:
+
+    def __init__(self, dataframe_input, sensitivity = 1.3):
+        df_generatedIndicators = pd.DataFrame() #Generated from indicator_generator
+
+        df_generatedSignal = pd.DataFrame() #Generated from signal_generation
+
+        df_trainTest = pd.DataFrame() #Generated from train_test
+        total_return = 0
+        return_potential_ratio = 0
+
+        self.dataframe_input = dataframe_input
+        self.sensitivity = sensitivity
+        
+#######################
+#Indicator Generator Function
+#######################
+
+    def indicator_generator(self):
+        df = self.dataframe_input
+        
+        df_indicators = pd.DataFrame()
+        df_indicators['DATE'] = df['DATE']
+        df_indicators['BOP'] =  (df['CLOSE'] - df['OPEN'])/(df['HIGH'] - df['LOW'])  
+
+        self.df_generatedIndicators = df_indicators
+    
+#######################
+#Signal Generation Dividers
+#######################
+
+    def signal_generation(self, indic_name = 'BOP'):
+        df = self.df_generatedIndicators
+        sensitivity = self.sensitivity
+        
+        mean = df['BOP'].mean()
+        std = df['BOP'].std()
+        long_gen = mean + sensitivity*std
+        short_gen = mean - sensitivity*std
+
+        indic_list = list(df['BOP'])
+        out_list = []
+        
+        for i in indic_list:
+            if (i >= long_gen):
+                out_list.append(100)
+            elif (i <= short_gen):
+                out_list.append(-100)
+            else:
+                out_list.append(0)
+                
+        df_out = pd.DataFrame()
+        df_out['DATE'] = df['DATE']
+        df_out['BOP SIGNAL'] = out_list
+        self.df_generatedSignal = df_out
+#######################
+#Train Test Function
+#######################
+
+    def train_test(self, indic_name = 'BOP', stop_percent = 0.05):
+        signum_colhead = indic_name + ' ' + 'SIGNAL'
+        df = self.dataframe_input
+        signal_df = self.df_generatedSignal
+
+        df_internal = pd.DataFrame()
+        df_internal['TYP PRICE'] = (df['OPEN'] + df['CLOSE'] + df['HIGH'] + df['LOW'])/4
+        df_internal['POSITION INDEX'] = [None for i in range(len(signal_df))]
+        
+        price_list = list(df_internal['TYP PRICE'])
+        signum_list = list(signal_df[signum_colhead])
+        
+        position_list = []
+        
+        position_flag = 'NEUTRAL'
+        entry_price = 0
+        long_book = [None for i in range(len(price_list))]
+        short_book = [None for i in range(len(price_list))]
+        
+        open_long = []
+        open_short = []
+        long_pos_list = []
+        short_pos_list = []
+        
+        for x in range(len(price_list)):
+            i = signum_list[x]
+            j = price_list[x]
+            if (x == (len(price_list) - 1)):
+                long_pos_list.append(open_long)
+                short_pos_list.append(open_short)
+                
+            if (position_flag == 'NEUTRAL'):   
+                if (i == 100):
+                    position_flag = 'LONG'
+                    entry_price = j
+                    long_book[x] = j
+                    open_long.append(j)
+                    continue 
+                elif (i == -100):
+                    position_flag = 'SHORT'
+                    entry_price = j
+                    short_book[x] = j
+                    open_short.append(j)
+                    continue
+                elif (i == 0):
+                    position_flag = 'NEUTRAL'
+                    continue
+            elif (position_flag == 'LONG'):
+                if (i == 100):
+                    open_long.append(j)
+                    trailing_stop = max(open_long) - max(open_long)*stop_percent
+                    absolute_stop = entry_price - entry_price*stop_percent
+                    if (j < absolute_stop or j < trailing_stop):
+                        position_flag = 'NEUTRAL'
+                        entry_price = 0
+                        long_pos_list.append(open_long)
+                        open_long = []
+                        continue
+                    else:
+                        position_flag = 'LONG'
+                        long_book[x] = j
+                        continue
+                elif (i == -100):
+                    position_flag = 'SHORT'
+                    entry_price = j
+                    short_book[x] = j
+                    long_pos_list.append(open_long)
+                    open_long = []
+                    continue
+                elif (i == 0):
+                    open_long.append(j)
+                    trailing_stop = max(open_long) - max(open_long)*stop_percent
+                    absolute_stop = entry_price - entry_price*stop_percent
+                    if (j < absolute_stop or j < trailing_stop):
+                        position_flag = 'NEUTRAL'
+                        entry_price = 0
+                        long_pos_list.append(open_long)
+                        open_long = []
+                        continue
+                    else:
+                        position_flag = 'LONG'
+                        long_book[x] = j
+                        continue
+            elif (position_flag == 'SHORT'):
+                if (i == 100):
+                    position_flag = 'LONG'
+                    entry_price = j
+                    long_book[x] = j
+                    short_pos_list.append(open_short)
+                    open_short = []
+                    continue 
+                elif (i == -100):
+                    open_short.append(j)
+                    trailing_stop = min(open_short) + max(open_short)*stop_percent
+                    absolute_stop = entry_price + entry_price*stop_percent
+                    if (j > absolute_stop or j > trailing_stop):
+                        position_flag = 'NEUTRAL'
+                        entry_price = 0
+                        short_pos_list.append(open_short)
+                        open_short = []
+                        continue
+                    else:
+                        position_flag = 'SHORT'
+                        short_book[x] = j
+                        continue
+                elif (i == 0):
+                    open_short.append(j)
+                    trailing_stop = min(open_short) + max(open_short)*stop_percent
+                    absolute_stop = entry_price + entry_price*stop_percent
+                    if (j > absolute_stop or j > trailing_stop):
+                        position_flag = 'NEUTRAL'
+                        entry_price = 0
+                        short_pos_list.append(open_short)
+                        open_short = []
+                        continue
+                    else:
+                        position_flag = 'SHORT'
+                        short_book[x] = j
+                        continue
+        long_return = 0
+        short_return = 0
+        for i in (long_pos_list):
+            if (i == []):
+                long_pos_list.remove(i)
+                continue
+            len_i = len(i) - 1
+            long_return += i[len_i] - i[0]
+            
+        for j in (short_pos_list):
+            if (j == []):
+                short_pos_list.remove(j)
+                continue
+            len_j = len(j) - 1
+            short_return += j[0] - j[len_j]
+            
+        total_return = long_return - short_return
+        possible_return = abs(price_list[0] - min(price_list)) + abs(max(price_list) - min(price_list)) + abs(price_list[-1] - max(price_list))
+        return_potential_ratio = total_return/possible_return
+        
+        df_internal['LONG BOOK'] = long_book
+        df_internal['SHORT BOOK'] = short_book
+        
+        self.total_return = total_return
+        self.return_potential_ratio = return_potential_ratio
+        self.df_trainTest = df_internal
+
+        return return_potential_ratio
+        
+#######################
+#Live Signal Generation Function
+#######################
+
+    def live_signal(self, live_lookback = 1):
+        indic_name = 'BOP'
+        mid_string = 'SIGNAL'
+        col_head = indic_name + ' ' + mid_string
+        out_list = []
+        for i in range(-live_lookback, 0):
+            out_list.append(self.df_generatedSignal[col_head].iloc[i])
+        return out_list
+
+#######################
+#Run Function
+#######################
+
+    def run(self, live_lookback = 1):
+        self.indicator_generator()
+        self.signal_generation()
+        weight = self.train_test()
+        live_signal = self.live_signal(live_lookback)
+
+        return weight, live_signal
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from StockDataExtraction.StockData import SingleStockData
 from IndicatorClasses.AccumulationDistribution import AccumulationDistribution
 from IndicatorClasses.Aroon import Aroon
 from IndicatorClasses.AverageTrueRange import AverageTrueRange
+from IndicatorClasses.BalanceOfPower import BalanceOfPower
 
 stock_data = SingleStockData(8,True, 200)
 stock_data.generate_dataframe()
 
-obj = AverageTrueRange(dataframe_input = stock_data.df, lookback_period = 5)
-y = obj.run(9)
+obj = BalanceOfPower(dataframe_input = stock_data.df)
+x,y = obj.run(9)
+print(x)
 print(y)

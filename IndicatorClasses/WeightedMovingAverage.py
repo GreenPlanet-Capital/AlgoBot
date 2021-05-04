11eb6e74c5dd6201229f8d2c5ffeb59b0d8ace8c
goldilock-zone <sarbajitghosh2002@gmail.com>
2021-05-04 16:39:47 +0530

    Documentation Changes

---
+++ IndicatorClasses/WeightedMovingAverage.py
@@ -0,0 +1,106 @@
+'''
+Name:
+
+Naming Convention of DataFrame Columns: 
+    Indicator Generated DataFrame head: 
+    Signal Generated DataFrame head: 
+    Signum Generated DataFrame head: 
+
+Function List:
+    indicator_generator
+    signal_generation
+    train_test
+    live_signal
+    run
+
+Type of Indicator: 
+
+Usage Notes:
+    
+'''
+'''
+Function Checklist
+- a function to take the dataframe input and clean it, in order to keep just the HIGH, LOW, CLOSE and VOLUME
+- indicator generation function
+- signal generation function
+- train test function, that returns the efficacy
+- current long/short strength 
+'''
+'''
+Inputs: dataframe_input, lookback_period, sensitivity = , absolute_sensitivity = 
+Outputs: weight, live_signal
+'''
+
+import math
+import pandas as pd
+import json 
+import datetime
+import numpy as np
+import sys
+import oauth2client
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.text
+
+class :
+
+    def __init__(self, dataframe_input, lookback_period, sensitivity = 1, absolute_sensitivity = 50):
+        df_generatedIndicators = pd.DataFrame() #Generated from indicator_generator
+
+        df_generatedSignal = pd.DataFrame() #Generated from signal_generation
+
+        df_trainTest = pd.DataFrame() #Generated from train_test
+        total_return = 0
+        return_potential_ratio = 0
+
+        self.dataframe_input = dataframe_input
+        self.lookback_period = lookback_period
+        self.sensitivity = sensitivity
+        self.absolute_sensitivity = absolute_sensitivity
+
+#######################
+#Indicator Generator Function
+#######################
+
+    def indicator_generator(self):
+
+#######################
+#Signal Generation Dividers
+#######################
+
+    def signal_generation(self, indic_name = ''):
+
+#######################
+#Train Test Function
+#######################
+
+    def train_test(self, indic_name = '', stop_percent = 0.05):
+
+#######################
+#Live Signal Generation Function
+#######################
+
+    def live_signal(self, live_lookback = 1):
+        indic_name = ''
+        mid_string = 'SIGNUM'
+        n = self.lookback_period
+        col_head = indic_name + ' ' + mid_string + ' ' + str(n)
+        out_list = []
+        for i in range(-live_lookback, 0):
+            out_list.append(self.df_generatedSignal[col_head].iloc[i])
+        return out_list
+
+#######################
+#Run Function
+#######################
+
+    def run(self, live_lookback = 1):
+        self.indicator_generator()
+        self.signal_generation()
+        weight = self.train_test()
+        live_signal = self.live_signal(live_lookback)
+
+        return weight, live_signal

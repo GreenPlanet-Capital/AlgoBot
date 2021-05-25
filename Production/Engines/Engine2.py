ffc577351d19ddbb6da9c7b249053f8c5912d7db
goldilock-zone <sarbajitghosh2002@gmail.com>
2021-05-25 19:00:18 +0530

    filters

--- Production/Engines/Engine2.py
+++ Production/Engines/Engine2.py
@@ -19,15 +19,15 @@ import multiprocessing as mp
 import math
 
 
-class Engine1:
+class Engine2:
     def __init__(self, dict_of_dataframes, base_lookback, number_of_readings):
         self.dict_of_dataframes = dict_of_dataframes
         self.base_lookback = base_lookback
         self.number_of_readings = number_of_readings
     
     def weight_adjust(self, input_weight):
-        if (input_weight <= 0.01):
-            return 0.01
+        if (input_weight <= 0.005):
+            return 0.005
         else:
             return input_weight
 
@@ -35,7 +35,8 @@ class Engine1:
         df_input['Typical Price'] = ((df_input['HIGH'] + df_input['LOW'] + df_input['CLOSE']) / 3).round(2)
         price_list = np.array(df_input['Typical Price'])
         lookback1 = self.base_lookback
-        multiplier1 = 1.3
+        multiplier1 = 1.7
+        reading1 = 0
 
         bol_mcg_obj = BollingerMcG(price_array = price_list , lookback = lookback1, multiplier = 2)
         bol_mcg_reading, bol_mcg_weight = bol_mcg_obj.run()
@@ -49,26 +50,42 @@ class Engine1:
         bol_wma_reading, bol_wma_weight = bol_wma_obj.run()
         bol_wma_weight = self.weight_adjust(bol_wma_weight)
 
-        env_mcg_obj = EnvelopeMcG(price_array = price_list , lookback = lookback1, multiplier = 0.03)
+        env_mcg_obj = EnvelopeMcG(price_array = price_list , lookback = lookback1, multiplier = 0.029)
         env_mcg_reading, env_mcg_weight = env_mcg_obj.run()
         env_mcg_weight = self.weight_adjust(env_mcg_weight)
 
         env_sma_obj = EnvelopeSMA(price_array = price_list , lookback = lookback1, multiplier = 0.025)
         env_sma_reading, env_sma_weight = env_sma_obj.run()
         env_sma_weight = self.weight_adjust(env_sma_weight)
-
+       
         env_wma_obj = EnvelopeWMA(price_array = price_list , lookback = lookback1, multiplier = 0.02)
         env_wma_reading, env_wma_weight = env_wma_obj.run()
         env_wma_weight = self.weight_adjust(env_wma_weight)
 
-        dm_dm_obj = DM_DM_Osc(price_array = price_list , short_lookback = lookback1, long_lookback = math.ceil(lookback1 * multiplier1))
-        dm_dm_reading, dm_dm_weight = dm_dm_obj.run()
-        dm_dm_weight = self.weight_adjust(dm_dm_weight)
+        lin_reg_obj = Lin_Reg(price_array = price_list, lookback = 3)
+        lin_reg_reading, lin_reg_weight = lin_reg_obj.run()
+        lin_reg_weight = self.weight_adjust(lin_reg_weight)
+        lin_reg_abs = lin_reg_obj.linreg_array[-1]
+
+        total_breakout = bol_mcg_reading + bol_sma_reading + bol_wma_reading + env_mcg_reading + env_sma_reading + env_wma_reading 
+        if (abs(total_breakout) < 500):
+            print("Throwing Value")
+            return 0
+
+        if (lin_reg_reading >= 0 and total_breakout > 0):
+            reading1 = 1
+        elif (lin_reg_reading >= 0 and total_breakout < 0):
+            print("Contradiction")
+            return 0
+        elif (lin_reg_reading <= 0 and total_breakout > 0):
+            print("Contradiction")
+            return 0
+        elif (lin_reg_reading <= 0 and total_breakout < 0):
+            reading1 = 1
 
         mcg_mcg_obj = McG_McG_Osc(price_array = price_list , short_lookback = lookback1, long_lookback = math.ceil(lookback1 * multiplier1))
         mcg_mcg_reading, mcg_mcg_weight = mcg_mcg_obj.run()
         mcg_mcg_weight = self.weight_adjust(mcg_mcg_weight)
-
 
         sma_mcg_obj = SMA_McG_Osc(price_array = price_list , short_lookback = lookback1, long_lookback = math.ceil(lookback1 * multiplier1))
         sma_mcg_reading, sma_mcg_weight = sma_mcg_obj.run()
@@ -90,19 +107,7 @@ class Engine1:
         wma_wma_reading, wma_wma_weight = wma_wma_obj.run()
         wma_wma_weight = self.weight_adjust(wma_wma_weight)
 
-        lin_reg_obj = Lin_Reg(price_array = price_list, lookback = 3)
-        lin_reg_reading, lin_reg_weight = lin_reg_obj.run()
-        lin_reg_weight = self.weight_adjust(lin_reg_weight)
-        lin_reg_abs = lin_reg_obj.linreg_array[-1]
-
-        total1 = (
-                bol_mcg_weight + 
-                bol_sma_weight +
-                bol_wma_weight +
-                env_sma_weight +
-                env_mcg_weight +
-                env_wma_weight + 
-                dm_dm_weight + 
+        total1 = (  
                 mcg_mcg_weight + 
                 sma_mcg_weight + 
                 sma_sma_weight + 
@@ -111,26 +116,29 @@ class Engine1:
                 wma_wma_weight
                 )
         
-        reading1 = (
-                (bol_mcg_reading * bol_mcg_weight +
-                bol_sma_reading * bol_sma_weight +
-                bol_wma_reading * bol_wma_weight + 
-                env_mcg_reading * bol_mcg_weight +
-                env_sma_reading * bol_sma_weight +
-                env_wma_reading * bol_wma_weight) + 
-                (dm_dm_reading * dm_dm_weight +
+        reading1 = reading1*(
                 sma_sma_reading * sma_sma_weight +
                 wma_wma_reading * wma_wma_weight + 
                 mcg_mcg_reading * mcg_mcg_weight +
                 sma_mcg_reading * sma_mcg_weight +
                 wma_sma_reading * wma_sma_weight + 
-                wma_mcg_reading * wma_mcg_weight) + 
-                lin_reg_reading * lin_reg_weight + 
-                lin_reg_abs * 10
+                wma_mcg_reading * wma_mcg_weight
                 )  
+
         reading = reading1/total1
 
-        return reading
+        if (reading >= 0 and total_breakout > 0):
+            return reading
+        elif (reading >= 0 and total_breakout < 0):
+            print("Contradiction1")
+            return 0
+        elif (reading <= 0 and total_breakout > 0):
+            print("Contradiction1")
+            return 0
+        elif (reading <= 0 and total_breakout < 0):
+            return reading
+        else:
+            return reading
 
     def generate_listOfTickers(self):
         in_dict = self.dict_of_dataframes

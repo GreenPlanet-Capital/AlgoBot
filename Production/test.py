71e24abf73fe2c0c1569bdef2bfabb131b70280b
Atharva Kale <atharvakale343@gmail.com>
2021-06-04 20:19:54 +0530

    test changes

--- Production/test.py
+++ Production/test.py
@@ -5,7 +5,7 @@ import sys
 from SecuritySelection.Baskets import Markets
 
 def main():
-    basket = Markets.snp_meme    
+    basket = Markets.nyse
     
     if len(sys.argv) > 1:
         if sys.argv[1] == '--update':
@@ -15,23 +15,21 @@ def main():
     
     begin = time.time()
     backtest1 = Backtester(
-
-        list_stock=basket,
-        initial_capital=1000000, 
-        base_lookback=5,
-        multiplier1=1.5, 
-        multiplier2=2, 
-        lin_reg_filter_multiplier=0.5, 
-        stop_loss_percent=0.05, 
-        filter_percentile=70, 
-        filter_activation_flag=True, 
-        long_only_flag=False, 
-        training_period=20, 
-        current_account_size_csv='intraday-test-trend',
-        update_data=False, 
-        percentRisk_PerTrade=0.1
-
-        )
+    list_stock=basket,
+    initial_capital=1000000, 
+    base_lookback=15,
+    multiplier1=1.5, 
+    multiplier2=2, 
+    lin_reg_filter_multiplier=0.5, 
+    stop_loss_percent=0.05, 
+    filter_percentile=80, 
+    filter_activation_flag=True, 
+    long_only_flag=False, 
+    training_period=50, 
+    current_account_size_csv='nse-intraday-test-trend',
+    update_data=update_data, 
+    percentRisk_PerTrade=0.02
+    )
     backtest1.run()
     end = time.time()
     print(f'Time taken for the backtest: {end - begin}')

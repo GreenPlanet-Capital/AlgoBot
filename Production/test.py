35015b4d61d78ec425de9263f240a69031a906a1
goldilock-zone <sarbajitghosh2002@gmail.com>
2021-06-04 12:13:19 +0530

    cleaning test.py

--- Production/test.py
+++ Production/test.py
@@ -14,7 +14,26 @@ def main():
         update_data = False
     
     begin = time.time()
-    backtest1 = Backtester(list_stock=basket, initial_capital=1000000, base_lookback=15, multiplier1=1.5, multiplier2=2, lin_reg_filter_multiplier=0.5, stop_loss_percent=0.05, filter_percentile=70, filter_activation_flag=True, long_only_flag=False, training_period=50, current_account_size_csv='long-term-trend', start_date="2021-01-01", end_date="2021-06-02", update_data=update_data, percentRisk_PerTrade=0.05)
+    backtest1 = Backtester(
+
+        list_stock=basket,
+        initial_capital=1000000, 
+        base_lookback=15,
+        multiplier1=1.5, 
+        multiplier2=2, 
+        lin_reg_filter_multiplier=0.5, 
+        stop_loss_percent=0.05, 
+        filter_percentile=70, 
+        filter_activation_flag=True, 
+        long_only_flag=False, 
+        training_period=50, 
+        current_account_size_csv='long-term-trend', 
+        start_date="2021-01-01", 
+        end_date="2021-06-02", 
+        update_data=update_data, 
+        percentRisk_PerTrade=0.05
+        
+        )
     backtest1.run()
     end = time.time()
     print(f'Time taken for the backtest: {end - begin}')
